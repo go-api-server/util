90ea9f6dbc783205a6a0d271b9b8a1dafddb835a
zhangys <zhangyangsheng@tom.com>
2019-07-13 17:45:20 +0800

    增加功能

--- mysql/querystring/select.go
+++ mysql/querystring/select.go
@@ -1,25 +1,36 @@
 package querystring
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 )
 
 type selectString struct {
+	fieldArray map[string]int
 	wherePtr   *whereString
-	fieldArray []string
+}
+
+func Count(table string) *selectString {
+	return &selectString{
+		fieldArray: make(map[string]int),
+		wherePtr:   newWhereString("SELECT", table, "COUNT(*)"),
+	}
 }
 
 func Select(fields string, from string) *selectString {
 	return &selectString{
-		wherePtr: newWhereString("SELECT", from, fields),
+		fieldArray: make(map[string]int),
+		wherePtr:   newWhereString("SELECT", from, fields),
 	}
 }
 
 func SelectArray(fieldArray []string, from string) *selectString {
 	return &selectString{
-		wherePtr: newWhereString("SELECT", from, strings.Join(fieldArray, ",")),
+		fieldArray: make(map[string]int),
+		wherePtr:   newWhereString("SELECT", from, strings.Join(fieldArray, ",")),
 	}
 }
 
@@ -38,7 +49,8 @@ func SelectObject(object string, from string) *selectString {
 		fieldArray = append(fieldArray, tagArray[0])
 	}
 	return &selectString{
-		wherePtr: newWhereString("SELECT", from, strings.Join(fieldArray, ",")),
+		fieldArray: make(map[string]int),
+		wherePtr:   newWhereString("SELECT", from, strings.Join(fieldArray, ",")),
 	}
 }
 
@@ -115,4 +127,107 @@ func (this *selectString) Limit(limit int64) *selectString {
 
 func (this *selectString) GetSQL() string {
 	return this.wherePtr.ToString()
+}
+
+func (this *selectString) Fetch(out interface{}, row *sql.Row) (bool, error) {
+	dest := reflect.ValueOf(out)
+
+	if dest.Type().Kind() == reflect.Ptr {
+		dest = dest.Elem()
+	}
+
+	vtype := dest.Type()
+	if vtype.Kind() != reflect.Struct {
+		return false, errors.New(fmt.Sprintf("dest: %s is not a struct", vtype.Name()))
+	}
+
+	this.fieldArray = make(map[string]int)
+	if this.wherePtr.fields != "*" {
+		fieldArray := strings.Split(this.wherePtr.fields, ",")
+		for index, field := range fieldArray {
+			this.fieldArray[field] = index
+		}
+	}
+
+	fieldCount := len(this.fieldArray)
+	scanArgs := make([]interface{}, fieldCount)
+
+	for i := 0; i < vtype.NumField(); i++ {
+		tag := vtype.Field(i).Tag.Get("db")
+		if len(tag) == 0 {
+			continue
+		}
+		arr := strings.Split(tag, ",")
+		key := arr[0]
+		if fieldCount > 0 {
+			idx, ok := this.fieldArray[key]
+			if !ok {
+				return false, errors.New(fmt.Sprintf("field: %s is not give", key))
+			}
+			scanArgs[idx] = dest.Field(i).Addr().Interface()
+		} else {
+			scanArgs = append(scanArgs, key)
+		}
+	}
+
+	err := row.Scan(scanArgs...)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return false, nil
+}
+
+func (this *selectString) FetchRows(out interface{}, rows *sql.Row) (bool, error) {
+	dest := reflect.ValueOf(out)
+
+	if dest.Type().Kind() == reflect.Ptr {
+		dest = dest.Elem()
+	}
+
+	vtype := dest.Type()
+	if vtype.Kind() != reflect.Struct {
+		return false, errors.New(fmt.Sprintf("dest: %s is not a struct", vtype.Name()))
+	}
+
+	if this.wherePtr.fields != "*" && len(this.fieldArray) == 0 {
+		fieldArray := strings.Split(this.wherePtr.fields, ",")
+		for index, field := range fieldArray {
+			this.fieldArray[field] = index
+		}
+	}
+
+	fieldCount := len(this.fieldArray)
+	scanArgs := make([]interface{}, fieldCount)
+
+	for i := 0; i < vtype.NumField(); i++ {
+		tag := vtype.Field(i).Tag.Get("db")
+		if len(tag) == 0 {
+			continue
+		}
+		arr := strings.Split(tag, ",")
+		key := arr[0]
+		if fieldCount > 0 {
+			idx, ok := this.fieldArray[key]
+			if !ok {
+				return false, errors.New(fmt.Sprintf("field: %s is not give", key))
+			}
+			scanArgs[idx] = dest.Field(i).Addr().Interface()
+		} else {
+			scanArgs = append(scanArgs, key)
+		}
+	}
+
+	err := rows.Scan(scanArgs...)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return false, nil
 }
